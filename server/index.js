ba9e192c402bfdc512bd05a395a7e37393108e95
Luis Gonzalez <luisalfredog@outlook.com>
2020-07-10 16:16:13 -0600

    Fix

--- server/index.js
+++ server/index.js
@@ -38,6 +38,6 @@ app.post('/video/token', (req, res) => {
   sendTokenResponse(token, res);
 });
 
-app.listen(5000, () =>
-  console.log('Express server is running on localhost:5000')
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
 );
