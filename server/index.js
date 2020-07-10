9f4c52ac7f2ce6c086dcc8e68f2bb31cb88b4c14
Luis Gonzalez <luisalfredog@outlook.com>
2020-07-10 17:18:41 -0600

    Fix

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ const sendTokenResponse = (token, res) => {
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   next();
 });
 
